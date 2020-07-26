5e1123110bfc9f40c9a3c5f0d38724d6bffaca2a
Sean Cone <seandoesitbetter@gmail.com>
2020-07-26 12:01:46 -0700

    Changing output to dist per project requirements

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const DIST_DIR = path.resolve(__dirname, "dist");
+const distPath = path.join(DIST_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
@@ -81,7 +81,7 @@ const internQuestions = () => {
                 initialQuestion();
             } else {
                 let data = render(employeeArr);
-                fs.writeFile(outputPath, data, (err) => {
+                fs.writeFile(distPath, data, (err) => {
                     if (err) throw err;
                     console.log('The file has been saved.');
                 });
@@ -129,7 +129,7 @@ const engineerQuestions = () => {
                 initialQuestion();
             } else {
                 let data = render(employeeArr);
-                fs.writeFile(outputPath, data, (err) => {
+                fs.writeFile(distPath, data, (err) => {
                     if (err) throw err;
                     console.log('The file has been saved!');
                 });
@@ -178,7 +178,7 @@ const managerQuestions = () => {
                 initialQuestion();
             } else {
                 let data = render(employeeArr);
-                fs.writeFile(outputPath, data, (err) => {
+                fs.writeFile(distPath, data, (err) => {
                     if (err) throw err;
                     console.log('The file has been saved!');
                 });
